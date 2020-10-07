abee6618a6603325c05290ac3deb50a8d90dc033
Muhammad Iqbaal <m.iqbaal2803@gmail.com>
2020-10-07 18:18:38 +0700

    tugas latihan storage fix

---
+++ app/src/main/java/com/iqbal/latihanstorage/MainActivity.java
@@ -0,0 +1,43 @@
+package com.iqbal.latihanstorage;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button internalStorage,eksternalStorage;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        internalStorage = findViewById(R.id.internalStorage);
+        eksternalStorage = findViewById(R.id.externalStorage);
+        internalStorage.setOnClickListener(this);
+        eksternalStorage.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.internalStorage:
+                Intent internal = new Intent(MainActivity.this, InternalActivity.class);
+                startActivity(internal);
+                break;
+            case R.id.externalStorage:
+                Intent eksternal = new Intent(MainActivity.this, EksternalActivity.class);
+                startActivity(eksternal);
+                break;
+        }
+
+    }
+
+
+}
